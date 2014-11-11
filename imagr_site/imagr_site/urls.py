54508782f93b11c15abb2265466f5d6f80ab195f
Charlie Rode <chrode10@gmail.com>
2014-11-11 10:30:57 -0800

    Nothing of import in this commit. Some whitespace deletion

--- imagr_site/imagr_site/urls.py
+++ imagr_site/imagr_site/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^imagr_app/', include('imagr_app.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls))
 )
