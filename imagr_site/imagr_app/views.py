7c0be33c093a979d94e0331750f0ffc5ad9fb8ea
Charlie Rode <chrode10@gmail.com>
2014-11-11 12:58:05 -0800

    Uncouple albums from photos

--- imagr_site/imagr_app/views.py
+++ imagr_site/imagr_app/views.py
@@ -32,23 +32,14 @@ def album_page(request, album_id):
 
 
 @login_required
-def photo_page(request, album_id, photo_id):
+def photo_page(request, photo_id):
     photo = Photo.objects.get(id=photo_id)
-    album = Album.objects.get(id=album_id)
-    context = {'album': album, 'photo': photo}
+    context = {'photo': photo}
     return render(request, 'imagr_app/photo.html', context)
 
 
 @login_required
 def stream(request):
-
     recent_user_photos = Photo.objects.filter(user=request.user.id).order_by('-date_uploaded')[:4]
-
-    for each_photo in recent_user_photos:
-        each_photo.album_id_list = []
-        for each_album_id in each_photo.Album_photos.id:
-            each_photo.album_id_list.append(each_album_id)
-
     return render(request, 'imagr_app/stream.html', {'recent_user_photos': recent_user_photos})
 
-    #return HttpResponse("Stream")
