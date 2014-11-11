981edd636669977e38c0cbaa7fc0ea25b1439e44
Charlie Rode <chrode10@gmail.com>
2014-11-11 14:41:55 -0800

    Add stream view function

--- imagr_site/imagr_app/views.py
+++ imagr_site/imagr_app/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 from django.shortcuts import get_object_or_404
 from models import ImagrUser, Photo, Album
 from django.contrib.auth.decorators import login_required
+import random
 
 
 def front_page(request):
@@ -25,7 +26,7 @@ def home_page(request):
 
 @login_required
 def album_page(request, album_id):
-    album = Album.objects.get(id=album_id)
+    album = get_object_or_404(Album, pk=album_id)
     photos = album.photos.all()
     context = {'album': album, 'photos': photos}
     return render(request, 'imagr_app/album.html', context)
@@ -41,5 +42,15 @@ def photo_page(request, photo_id):
 @login_required
 def stream(request):
     recent_user_photos = Photo.objects.filter(user=request.user.id).order_by('-date_uploaded')[:4]
-    return render(request, 'imagr_app/stream.html', {'recent_user_photos': recent_user_photos})
+    user = get_object_or_404(get_user_model(), pk=request.user.id)
+    recent_friend_photos = []
 
+    for photo in Photo.objects.exclude(published="private").order_by('-date_uploaded')[:4]:
+        for follower in photo.user.followers.all():
+            if request.user.id == follower.id:
+                recent_friend_photos.append(photo)
+
+    photos_to_stream = random.sample(recent_friend_photos, min(len(recent_friend_photos), 4))
+    context = {'friends_photos': photos_to_stream, 'user_photos': recent_user_photos}
+
+    return render(request, 'imagr_app/stream.html', context)
