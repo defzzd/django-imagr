72e4b7a36a8c97dda274761b07bc2e22ea070935
Charlie Rode <chrode10@gmail.com>
2014-11-11 10:19:51 -0800

    Add accounts/login path to URLconf

--- imagr_site/imagr_app/urls.py
+++ imagr_site/imagr_app/urls.py
@@ -6,8 +6,10 @@ from imagr_app import views
 urlpatterns = patterns('',
     url(r'^$', views.front_page, name='front_page'),
 
-    # Alternate root page
+    # Alternate root page here, perhaps?
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'imagr_app/login.html'}),
     url(r'^album/(?P<album_id>\d+)$', views.album_page, name='album_page'),
     url(r'^album/(?P<album_id>\d+)/photo/(?P<photo_id>\d+)$', views.photo_page, name='photo_page'),
     url(r'^stream$', views.stream, name='stream')
+
 )
