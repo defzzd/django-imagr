f30772cd557c9894a694cbcf6ef1f3cea09fd0a8
CharlesGust <charlesg@equality.net>
2014-11-18 15:26:39 -0800

    add database test cases

--- imagr_site/imagr_app/tests.py
+++ imagr_site/imagr_app/tests.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.test import TestCase, Client, RequestFactory
+import unittest
+from django.test import TestCase, SimpleTestCase, TransactionTestCase
+from django.test import Client, RequestFactory
 from imagr_app.models import Photo, Album, ImagrUser
+from django.test.utils import setup_test_environment, teardown_test_environment
+
+# This workaround suggested by dghubble at:
+# http://stackoverflow.com/questions/17912060/django-test-client-always-returning-301
+# when 301 errors are being returned on pages that re-direct to SSL pages
+# from django.test.utils import override_settings
 
 # Thanks to Brandon at:
 #  http://stackoverflow.com/questions/15073227/django-unit-test-simple-example
@@ -9,7 +17,7 @@ from imagr_app.models import Photo, Album, ImagrUser
 # Create your tests here.
 
 
-class ClientUsesImagr(TestCase):
+class testImagrTestCase(TestCase):
     fixtures = ['imagr_app_testdata.json']
 
     def setUp(self):
@@ -17,3 +25,57 @@ class ClientUsesImagr(TestCase):
 
     def test_empty_datas(self):
         self.client.login(username='', password='')
+
+
+# TransactionTestCase's need:
+#  from django.test import TransactionTestCase
+class testImagrTransactionTestCase(TransactionTestCase):
+    fixtures = ['imagr_app_testdata.json']
+
+    def test_accessRestrictedURLS(self):
+        response = self.client.post('/accounts/login/', {
+            'username': 'admin', 'password': 'admin'})
+        self.assertEqual(response.status_code, 302)
+        # print response
+
+        response = self.client.get('/imagr_app/album/1/')
+        self.assertEqual(response.status_code, 200)
+        # print response
+
+        response = self.client.get('/imagr_app/photo/8/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/imagr_app/photo/9/')
+        self.assertEqual(response.status_code, 404)
+
+
+# SimpleTestCase's need:
+#  from django.test import SimpleTestCase
+
+# The override decorator was suggested as a fix to unknown 301 status codes
+# @override_settings(SSLIFY_DISABLE=True)
+class testImagrSimpleTestCase(SimpleTestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def tearDown(self):
+        pass
+
+    def test_accessURLS(self):
+
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/imagr_app/')
+        self.assertEqual(response.status_code, 200)
+
+
+# These test cases should be simple enough to require no Django
+class testImagrunittestTestCase(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    setup_test_environment()
+    unittest.main()
+    teardown_test_environment()
